819236e9d91ff09ef4aa76f1f70d925068bcae41
Juan V <jjsiete@gmail.com>
2023-04-19 23:35:07 +0200

    limit player forward movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 public class PlayerController : MovingObject
 {
     [SerializeField] private int health = 100;
+    [SerializeField] private int zBoundaryMax = 0;
     private Rigidbody playerRigidBody;
     private readonly float horizontal_speed = 30.0f;
     private readonly float vertical_speed = 20.0f;
     GameManager gameManager;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         gameManager.NotifyHealthChange(health);
-        playerRigidBody = GetComponent<Rigidbody>();   
+        playerRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,7 +25,15 @@ public class PlayerController : MovingObject
         if (!gameManager.IsGameActive) return;
 
         playerRigidBody.AddForce(Input.GetAxis("Horizontal") * horizontal_speed * Vector3.right);
-        playerRigidBody.AddForce(Input.GetAxis("Vertical") * vertical_speed * Vector3.forward);
+        if (transform.position.z > zBoundaryMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundaryMax);
+        }
+        else 
+        { 
+            playerRigidBody.AddForce(Input.GetAxis("Vertical") * vertical_speed * Vector3.forward); 
+        }
+
     }
 
     public void TakeDamage(int damage)
