679fbdda5d37ddee6743f6bc92be2b049497c75f
Juan V <jjsiete@gmail.com>
2023-04-24 23:35:37 +0200

    smooth road textures repeating

--- Assets/Scripts/MoveRoad.cs
+++ Assets/Scripts/MoveRoad.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MoveRoad : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
     private Vector3 initialPosition;
+    private float repeatingWidth;
 
     // Start is called before the first frame update
     void Start()
     {
         initialPosition = transform.position;
+        repeatingWidth = GetComponent<BoxCollider>().size.z / 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z < initialPosition.z - 50.0f)
+        if (transform.position.z < initialPosition.z - repeatingWidth)
         {
             transform.position = initialPosition;
         }
